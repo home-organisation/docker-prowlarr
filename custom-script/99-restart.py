5b542827f3719241b1b142afea78eac1b68350f0
bizalu <chprok@gmail.com>
2023-03-29 14:54:31 +0200

    debug restart

--- custom-script/99-restart.py
+++ custom-script/99-restart.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import requests
 import os
 import logging
 
@@ -7,37 +6,18 @@ import logging
 # SET STATIC CONFIG
 ###########################################################
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
-PROWLARR_URL = 'http://localhost:9696'
 
 
 ###########################################################
 # DEFINE FUNCTION
 ###########################################################
-def restart(url, apikey):
-    api_url = url + "/api/v3/system/restart"
-    api_header = {'accept': '*/*', 'X-Api-Key': apikey}
-
-    try:
-        response = requests.post(api_url, headers=api_header)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as errh:
-        logging.error(errh)
-    except requests.exceptions.ConnectionError as errc:
-        logging.error(errc)
-    except requests.exceptions.Timeout as errt:
-        logging.error(errt)
-    except requests.exceptions.RequestException as err:
-        logging.error(err)
-
-    return response.status_code
+def restart():
+    os.popen('s6-svc -r /var/run/s6-rc/servicedirs/svc-prowlarr/')
 
 
 ###########################################################
 # INIT CONFIG
 ###########################################################
 if __name__ == '__main__':
-    with open("/tmp/old-apikey", mode="rt") as f:
-        APIKEY = f.readline()
-
-    logging.info("Restart application %s ..." % PROWLARR_URL)
-    restart(PROWLARR_URL, APIKEY)
+    logging.info("Restart application ...")
+    restart()
