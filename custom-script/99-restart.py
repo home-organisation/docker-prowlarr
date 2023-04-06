51072acac5139bf89fdff8cdc4ef5ccd9b74dbab
bizalu <chprok@gmail.com>
2023-04-06 12:38:19 +0200

    debug restart

--- custom-script/99-restart.py
+++ custom-script/99-restart.py
@@ -7,7 +7,7 @@ import sqlite3
 # SET STATIC CONFIG
 ###########################################################
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
-PROWLARR_DB = 'config/prowlarr.db'
+PROWLARR_DB = '/config/prowlarr.db'
 
 
 ###########################################################
