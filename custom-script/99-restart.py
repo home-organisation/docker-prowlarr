ae8362900e0b1082178973bc9c65a286cd812351
bizalu <chprok@gmail.com>
2023-07-24 17:45:24 +0200

    retest pipeline slack integration

--- custom-script/99-restart.py
+++ custom-script/99-restart.py
@@ -9,7 +9,6 @@ import sqlite3
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 PROWLARR_DB = '/config/prowlarr.db'
 
-
 ###########################################################
 # DEFINE FUNCTION
 ###########################################################
