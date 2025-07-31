11fcdc58c7321ec3d46756096d526d5ee8625372
bizalu <chprok@gmail.com>
2025-07-31 21:04:42 +0200

    Add delay and update /etc/hosts for yggtorrent in init script; bump version to 2025.7.31-2102

--- scripts/10-init-config.sh
+++ scripts/10-init-config.sh
@@ -2,3 +2,8 @@
 # shellcheck shell=bash
 
 python /app/init/init.py
+
+sleep 5
+ip=$(nslookup www.yggtorrent.top 192.168.1.21 | grep Address | awk '{ print $2 }' | sed -n 2p)
+echo "Adding www.yggtorrent.top to /etc/hosts with IP ${ip}"
+echo "${ip}    www.yggtorrent.top" >> /etc/hosts
